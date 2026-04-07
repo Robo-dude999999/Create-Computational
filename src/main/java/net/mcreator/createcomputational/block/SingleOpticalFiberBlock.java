bb1b36b5a25c167f4bd6458a731ed507d26091c5
Robo-dude999999 <cd.kennedy3.1459@gmail.com>
2026-04-07 17:30:27 -0400

    changed sof rotation properties

--- src/main/java/net/mcreator/createcomputational/block/SingleOpticalFiberBlock.java
+++ src/main/java/net/mcreator/createcomputational/block/SingleOpticalFiberBlock.java
@@ -3,16 +3,14 @@ package net.mcreator.createcomputational.block;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.DirectionalBlock;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -24,12 +22,13 @@ import net.minecraft.core.BlockPos;
 import net.mcreator.createcomputational.procedures.TestTransferOnTickUpdateProcedure;
 
 public class SingleOpticalFiberBlock extends Block {
-	public static final DirectionProperty FACING = DirectionalBlock.FACING;
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+	public static final EnumProperty<AttachFace> FACE = FaceAttachedHorizontalDirectionalBlock.FACE;
 	public static final BooleanProperty TRANSFER = BooleanProperty.create("transfer");
 
 	public SingleOpticalFiberBlock() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.GLASS).strength(1f, 10f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
-		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(TRANSFER, false));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(FACE, AttachFace.WALL).setValue(TRANSFER, false));
 	}
 
 	@Override
@@ -50,24 +49,38 @@ public class SingleOpticalFiberBlock extends Block {
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
 		return switch (state.getValue(FACING)) {
-			default -> box(7.5, 0, 0, 8.5, 1, 16);
-			case NORTH -> box(7.5, 0, 0, 8.5, 1, 16);
-			case EAST -> box(0, 0, 7.5, 16, 1, 8.5);
-			case WEST -> box(0, 0, 7.5, 16, 1, 8.5);
-			case UP -> box(7.5, 0, 0, 8.5, 16, 1);
-			case DOWN -> box(7.5, 0, 15, 8.5, 16, 16);
+			default -> switch (state.getValue(FACE)) {
+				case FLOOR -> box(7.5, 0, 0, 8.5, 1, 16);
+				case WALL -> box(7.5, 0, 0, 8.5, 16, 1);
+				case CEILING -> box(7.5, 15, 0, 8.5, 16, 16);
+			};
+			case NORTH -> switch (state.getValue(FACE)) {
+				case FLOOR -> box(7.5, 0, 0, 8.5, 1, 16);
+				case WALL -> box(7.5, 0, 15, 8.5, 16, 16);
+				case CEILING -> box(7.5, 15, 0, 8.5, 16, 16);
+			};
+			case EAST -> switch (state.getValue(FACE)) {
+				case FLOOR -> box(0, 0, 7.5, 16, 1, 8.5);
+				case WALL -> box(0, 0, 7.5, 1, 16, 8.5);
+				case CEILING -> box(0, 15, 7.5, 16, 16, 8.5);
+			};
+			case WEST -> switch (state.getValue(FACE)) {
+				case FLOOR -> box(0, 0, 7.5, 16, 1, 8.5);
+				case WALL -> box(15, 0, 7.5, 16, 16, 8.5);
+				case CEILING -> box(0, 15, 7.5, 16, 16, 8.5);
+			};
 		};
 	}
 
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
-		builder.add(FACING, TRANSFER);
+		builder.add(FACING, FACE, TRANSFER);
 	}
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return super.getStateForPlacement(context).setValue(FACING, context.getClickedFace()).setValue(TRANSFER, false);
+		return super.getStateForPlacement(context).setValue(FACE, faceForDirection(context.getNearestLookingDirection())).setValue(FACING, context.getHorizontalDirection().getOpposite()).setValue(TRANSFER, false);
 	}
 
 	public BlockState rotate(BlockState state, Rotation rot) {
@@ -76,6 +89,13 @@ public class SingleOpticalFiberBlock extends Block {
 
 	public BlockState mirror(BlockState state, Mirror mirrorIn) {
 		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
+	}
+
+	private AttachFace faceForDirection(Direction direction) {
+		if (direction.getAxis() == Direction.Axis.Y)
+			return direction == Direction.UP ? AttachFace.CEILING : AttachFace.FLOOR;
+		else
+			return AttachFace.WALL;
 	}
 
 	@Override
